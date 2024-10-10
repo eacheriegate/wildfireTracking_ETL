2913ffba9168bf01202fd731d9c9f39a70fa81a4
Emma Cheriegate <eacheriegate@gmail.com>
2024-10-10 16:10:15 -0700

    update workflow indentations

--- scripts/update_webmap_daily.py
+++
@@ -1,7 +0,0 @@
-import os
-
-# Run each step of the ETL and visualization workflow
-os.system("python scripts/extract.py")
-os.system("python scripts/transform.py")
-os.system("python scripts/visualize.py")
-
